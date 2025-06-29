62827e49c4b2ae9692a989f98134550d8fc13367
Tata <youchoudeyu@qq.com>
2025-06-29 14:22:38 +0800

    fix the chatbot hidden problem

--- src/newtab/components/TabCards.tsx
+++ src/newtab/components/TabCards.tsx
@@ -221,7 +221,7 @@ export default function TabCards({ tabs, searchQuery = '', onTabClick, onTabClos
       </div>
 
       {/* 右侧 AI 对话 */}
-      <div className="w-180 min-w-0">
+      <div className="flex-1 w-180 min-w-0">
         <AIChatBox
           currentTabs={tabs}
           onAnalyzeTabs={handleTabsAnalyzed}
